e25086fcad6a7d67e10da18ada5a621135b130cc
Hoyeom <korehdrb@gmail.com>
2022-05-24 13:51:41 +0900

    Health Bar Anim

--- Assets/Scripts/Runtime/UI/UI_Health.cs
+++ Assets/Scripts/Runtime/UI/UI_Health.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,8 @@ namespace Runtime.UI
     public class UI_Health : MonoBehaviour
     {
         [SerializeField] private Image _image;
-        
+        [SerializeField] private Color _color;
+        private float temp = float.MaxValue;
         private void Start()
         {
             Managers.Game.Player.OnChangeHealth += OnChangeHealth;
@@ -16,6 +18,14 @@ namespace Runtime.UI
         private void OnChangeHealth(float cur, float max)
         {
             _image.fillAmount = cur / max;
+
+            if (cur > temp)
+            {
+                _image.color = Color.white;
+                _image.DOColor(_color, .2f).Restart();
+            }
+
+            temp = cur;
         }
     }
 }
